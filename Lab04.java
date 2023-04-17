413ecbdb2454c0282a4a317584fe69ee3b1f9c26
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-04-17 10:13:00 -0700

    only missing javadoc

--- Lab04.java
+++ Lab04.java
@@ -1,25 +1,52 @@
 import java.util.Random;
+import java.util.Scanner;
 
 public class Lab04 {
     
-        static String[] articles = {"the", "a", "one", "some", "any"};
-        static String[] nouns = {"person", "man", "woman", "dog", "cat", "city", "car", "bicycle"};
-        static String[] verbs = {"ran", "walked", "jumped", "skipped", "traveled", "drove"};
-        static String[] prepositions = {"to", "from", "over", "under", "on", "by", "for", "away", "towards", "around", "near"};
-        public static void main(String[] args) {
+    static String[] articles = {"the", "a", "one", "some", "any", "every", "each", "several"};
+    static String[] nouns = {"person", "man", "woman", "dog", "cat", "city", "car", "bicycle", "tree", "mountain", "river", "ocean", "sun", "moon"};
+    static String[] verbs = {"ran", "walked", "jumped", "skipped", "traveled", "drove", "swam", "flew", "climbed", "danced", "sang", "played"};
+    static String[] prepositions = {"to", "from", "over", "under", "on", "by", "for", "away", "towards", "around", "near", "among", "within", "without"};
+    
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println(generateSentence());
+        do {
+            int sentenceAmount = 0;
 
+            while (true) {
+                System.out.println("Pedro and Matt's Sentence Generator using StringBuilder (Lab04)");
+                System.out.println("How many sentences do you want? Between (1-25): ");
+                sentenceAmount = scanner.nextInt();
+                if (sentenceAmount >= 1 && sentenceAmount <= 25) {
+                    break;
+                }
+                System.out.println("Invalid Input");
+            }
 
+            StringBuilder allSentences = new StringBuilder();
+
+            for (int i = 0; i < sentenceAmount; i++) {
+                StringBuilder sentence = generateSentence();
+                allSentences.append((i+1) + ". " + sentence + "\n");
+            }
+
+            System.out.println(allSentences.toString());
+
+            System.out.println("Generate more sentences? (Y/N)");
+        } while (scanner.next().equalsIgnoreCase("Y"));
     }
 
     public static StringBuilder generateSentence() {
-        
         StringBuilder sentence = new StringBuilder();
         Random random = new Random();
+        String firstWord;
 
         int randomWord = random.nextInt(articles.length);
-        sentence.append(articles[randomWord]);
+        firstWord = articles[randomWord];
+        sentence.append(firstWord.substring(0, 1).toUpperCase());
+        sentence.append(firstWord.substring(1));
         sentence.append(" ");
 
         randomWord = random.nextInt(nouns.length);
